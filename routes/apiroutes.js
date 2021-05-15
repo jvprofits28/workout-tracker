74a20bd7c525f9092981ee0a8dc7f2ddf6ef0348
Juan Vazquez <ehulises28@hotmail.com>
2021-05-15 11:24:47 -1000

    added apiroutes

---
+++ routes/apiroutes.js
@@ -0,0 +1,40 @@
+const router = require("express").router();
+const db = require("../models/workout");
+
+router.get("api/workouts", (req, res) => {
+  db.find()
+    .then((dbworkout) => {
+      res.json(dbworkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+router.get("/api/workouts/range", (req, res) => {
+  db.find()
+    .then((dbData) => {
+      res.json(dbData);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+router.post("/api/workouts", ({ body }, res) => {
+  db.create(body)
+    .then((dbworkout) => {
+      res.json(dbworkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  db.findByIdAndUpdate(params.id, { $push: { exercises: body } })
+    .then((dbworkout) => {
+      res.json(dbworkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+module.exports = router;
